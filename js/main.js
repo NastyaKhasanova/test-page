7275f4aab8361856022c74cec37630c80067947e
NastyaKhasanova <nastja.bast@yandex.ru>
2020-02-24 23:28:24 +0500

    last

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+function prepare (){
   $('.nav>li>a').on('click', function(e){
     e.preventDefault();
     var id = $(this).attr('href');
@@ -10,4 +10,13 @@ $(document).ready(function(){
     $(this).find('.toggle-text').css('opacity', '0');
     $(this).find('.tel').addClass('active');
   });
-});
+}
+
+function loadScript() {
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
+script.onload=prepare;
+document.body.appendChild(script);
+}
+window.onload=loadScript;
